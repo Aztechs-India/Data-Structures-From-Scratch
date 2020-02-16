ceabe98d7e818e6ecbcee07570035e446f4ab5c6
Deepak Acharya <aacharyadeepak100@gmail.com>
2020-02-16 21:02:40 +0530

    Create Array Rotation Template

---
+++ src/com/geeksforgeeks/arrays/SimpleArrayRotation.java
@@ -0,0 +1,13 @@
+package com.geeksforgeeks.arrays;
+
+import com.geeksforgeeks.interfaces.DataStructuresAlgorithms;
+
+public class SimpleArrayRotation implements DataStructuresAlgorithms<Void, Integer[]> {
+
+	@Override
+	public Void getOutput(Integer[] input) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
